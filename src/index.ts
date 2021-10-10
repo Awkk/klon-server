67a2ad2a124b817aa0bb61e7b0e0431b93cf6170
Andrew Wong <kkwong125@gmail.com>
2021-10-09 21:18:15 -0700

    Set up Apollo Server, TypeGraphQL and Express

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,38 @@
 import "dotenv/config";
+import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { Post } from "./entities/Post";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { PostResolver } from "./resolvers/post";
 
 const main = async () => {
-  const connection = await createConnection({
+  // PostgreSQL connection
+  await createConnection({
     type: "postgres",
     url: process.env.DATABASE_URL,
     logging: true,
     synchronize: true,
     entities: [Post],
+  });
+
+  // Express
+  const app = express();
+  const port = process.env.PORT;
+
+  // Apollo Server
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+  });
+  await apolloServer.start();
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(port, () => {
+    console.log("server listening on port:", port);
   });
 };
 
