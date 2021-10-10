d3fd6abe5d577ef4ba2ff30d8fe0e0274bef9d7e
Andrew Wong <kkwong125@gmail.com>
2021-10-09 19:48:16 -0700

    Created postgresql connection

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import "dotenv/config";
+import { createConnection } from "typeorm";
+
 const main = async () => {
-    console.log("hello world");
+  const connection = await createConnection({
+    type: "postgres",
+    url: process.env.DATABASE_URL,
+    logging: true,
+  });
+  console.log(connection.isConnected);
 };
 
 main();
