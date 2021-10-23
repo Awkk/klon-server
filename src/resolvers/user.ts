b0ae84806bd65fc50ce3d71d8ffc8447ee48eb20
Andrew Wong <kkwong125@gmail.com>
2021-10-22 22:22:47 -0700

    Updated auth validation and error messages

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -45,7 +45,7 @@ export class UserResolver {
         // unique username violation
         if (err.code === "23505") {
           return {
-            errors: [{ field: "username", message: "username already taken" }],
+            errors: [{ field: "username", message: "Username already taken" }],
           };
         }
       }
@@ -63,12 +63,12 @@ export class UserResolver {
   ): Promise<UserResponse> {
     const user = await User.findOne({ where: { username: auth.username } });
     if (!user) {
-      return { errors: [{ field: "username", message: "user not exist" }] };
+      return { errors: [{ field: "username", message: "User not exist" }] };
     }
 
     const validPw = await argon2.verify(user.password, auth.password);
     if (!validPw) {
-      return { errors: [{ field: "password", message: "incorrect password" }] };
+      return { errors: [{ field: "password", message: "Incorrect password" }] };
     }
     // create login session and send cookie
     req.session.userId = user.id;
--- src/utils/validateAuth.ts
+++ src/utils/validateAuth.ts
@@ -6,14 +6,24 @@ export const validateAuth = (auth: UserAuthInput): FieldError[] => {
   if (auth.username.length < 3) {
     error.push({
       field: "username",
-      message: "username must be at least 3 characters long",
+      message: "Must be at least 3 characters long",
+    });
+  } else if (auth.username.length > 20) {
+    error.push({
+      field: "username",
+      message: "Must be at most 20 characters long",
     });
   }
 
   if (auth.password.length < 3) {
     error.push({
       field: "password",
-      message: "password must be at least 3 characters long",
+      message: "Must be at least 3 characters long",
+    });
+  } else if (auth.username.length > 128) {
+    error.push({
+      field: "password",
+      message: "Must be at most 128 characters long",
     });
   }
 
